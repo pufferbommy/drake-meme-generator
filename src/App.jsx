8e9054cbc4f8733475b600e271c1a23c5f2abbc4
pufferbommy <pufferbommy@gmail.com>
2023-03-06 19:39:38 +0700

    show drake meme in canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import styles from './styles/App.module.css'
+import { useEffect, useRef } from 'react'
+import drakeMemeTemplate from './images/drake-meme-template.jpg'
+
 function App() {
-  return <div>app</div>
+  const canvasRef = useRef()
+
+  useEffect(function () {
+    const canvasContext = canvasRef.current.getContext('2d')
+    const image = new Image()
+    image.src = drakeMemeTemplate
+    image.onload = function () {
+      canvasContext.drawImage(image, 0, 0)
+    }
+  }, [])
+
+  return (
+    <div className={styles.App}>
+      <canvas ref={canvasRef} width={750} height={750} />
+    </div>
+  )
 }
 
 export default App
